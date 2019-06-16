5ae22e13e75bf7a369cfd2c930c64080800ee917
Vamsi Ravi <Vamsi.Ravi@hilton.com>
2019-06-15 20:23:34 -0500

    Initial Implementation of to send SMS

---
+++ twiliosmsimplementation/src/main/java/com/twilio/sms/implementation/TwiliosmsimplementationApplication.java
@@ -0,0 +1,13 @@
+package com.twilio.sms.implementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwiliosmsimplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwiliosmsimplementationApplication.class, args);
+	}
+
+}
